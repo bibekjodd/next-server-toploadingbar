1f75e00531304681ad63a21c84502cec4ef37f6d
bibekjodd <bibekjodd@outlook.com>
2023-10-05 21:22:10 +0545

    fix: remove initial loading bar animation

--- src/components/TopLoadingBar.tsx
+++ src/components/TopLoadingBar.tsx
@@ -7,12 +7,15 @@ import LoadingBar from "react-top-loading-bar";
 export default function TopLoadingBar() {
   const progress = useLoadingBar((state) => state.progress);
   const end = useLoadingBar((state) => state.end);
+  const initial = useLoadingBar((state) => state.initial);
+  const initialLoaded = useLoadingBar((state) => state.initialLoaded);
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    end();
-  }, [pathname, searchParams, end]);
+    if (initial) initialLoaded();
+    else end();
+  }, [pathname, searchParams]);
 
   return <LoadingBar progress={progress} waitingTime={200} />;
 }
--- src/hooks/useLoadingBar.ts
+++ src/hooks/useLoadingBar.ts
@@ -1,13 +1,21 @@
 import { create } from "zustand";
 
 interface UseLoadingBar {
+  initial: boolean;
   progress: number;
+
+  initialLoaded: () => void;
   start: () => void;
   end: () => void;
 }
 
 const useLoadingBar = create<UseLoadingBar>((set) => ({
   progress: 0,
+  initial: true,
+
+  initialLoaded() {
+    set({ initial: false });
+  },
   start() {
     set({ progress: 90 });
   },
